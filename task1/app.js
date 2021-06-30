c4f9eb4416f40bdfb9750becfd24a14bb4455026
Muhammad-Hamza <77409746+MuhammadHamza-101@users.noreply.github.com>
2021-06-30 22:38:31 +0500

    Add files via upload

--- task1/app.js
+++ task1/app.js
@@ -15,16 +15,37 @@
 
   localStorage.setItem('user_data',JSON.stringify(user_list));
 
-  
 }
 
 get_LocalStorage =  ()=> {
-
+  
   let getUserList = localStorage.getItem('user_data');
-  let heading = document.getElementById('paragraph')
-  let j = getUserList
-  let resut = JSON.parse(j)
-  heading.innerHTML = resut;
+  let convert = getUserList
+  let resut = JSON.parse(convert)
+  
+  let user_name = document.getElementById('name')
+  let user_fname = document.getElementById('fname')
+  let user_gmail = document.getElementById('gmail')
+  let user_phone = document.getElementById('phone')
+  
+  user_name.innerHTML = resut.name;
+  user_fname.innerHTML = resut. fatherName;
+  user_gmail.innerHTML = resut.gmail;
+  user_phone.innerHTML = resut. phone;
   console.log (resut)
   
 }
+
+// let username = document.getElementById('Name').value
+//   let fatherName = document.getElementById('Father_Name').value;
+//   let userGamil = document.getElementById('Gmail').value;
+//   let userNumber = document.getElementById('Number').value;
+
+
+
+// let btn =  document.getElementById('clear')
+
+// .addEventListener('click',()=>{
+ 
+//   username.value = " remove ";
+// })
